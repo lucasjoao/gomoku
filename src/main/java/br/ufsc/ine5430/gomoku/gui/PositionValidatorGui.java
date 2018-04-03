cafeecbb50bf774a85eab4ae518312ecea5e2dd6
Christian Pieri <chris@bridge.ufsc.br>
2018-04-02 22:53:42 -0300

    javadoc validador de posicao gui

--- src/main/java/br/ufsc/ine5430/gomoku/gui/PositionValidatorGui.java
+++ src/main/java/br/ufsc/ine5430/gomoku/gui/PositionValidatorGui.java
@@ -6,8 +6,20 @@ import javax.swing.JOptionPane;
 
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Classe de validação para verificar se o número passado como linha e coluna são vålidos
+ */
 public class PositionValidatorGui {
 
+	/**
+	 * O tabuleiro é composto de 225 casas, cada qual com sua chave e valor.
+	 * A chave é o ID sequencial e único.
+	 * O valor é o que está dentro da casa: - (vazio); x (humano) ou o (máquina).
+	 * A validação contempla valores OK para linha e coluna, bem como avisa caso uma casa já fora ocupada anteriormente.
+	 * @param tabuleiro
+	 * @param valor
+	 * @return true se nenhum erro for encontrado ou false caso contrário (boolean)
+	 */
 	public static boolean check(Map<Integer, String> tabuleiro, int valor) {
 		if (valor < 1 || valor > 225) {
 			JOptionPane.showMessageDialog(null, "Valor inválido! Escolha outro.");
@@ -22,6 +34,11 @@ public class PositionValidatorGui {
 		}
 	}
 
+	/**
+	 * Verifica se a determinada String passada como parâmetro é um numeral
+	 * @param input
+	 * @return o valor correspondente por essa String, caso seja um numeral; ou o menor valor Integer existente, caso contrário (int)
+	 */
 	public static int checkInput(String input) {
 		return StringUtils.isNumeric(input) ? Integer.valueOf(input) : Integer.MIN_VALUE;
 	}
